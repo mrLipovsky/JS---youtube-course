d693c72297a46d39c46793e1e94cdc8324a085f7
mr_lipovsky <90620664+mrLipovsky@users.noreply.github.com>
2022-11-06 14:12:19 +0100

    Add files via upload

--- JS - youtube course/script.js
+++ JS - youtube course/script.js
@@ -1,3 +1,4 @@
+//variables
 let nameOne = 'peter';
 console.log(nameOne);
 
@@ -13,7 +14,82 @@ console.log(ageOne);
 
 let name = 'Peter'; //string
 let age = 36; //number
-let isApproved = true; //boolien
+let isApproved = true; //boolean
 let firstName = undefined; //undefined variable 
 let date = null; //null object 
 
+// Objects
+let girl = {
+    name: 'Emily',
+    age: 23,
+    eyeColor: 'brown'
+};
+girl.name = 'Peter';
+girl['name'] = 'Sam';
+console.log(girl);
+
+// Arrays 
+let items = ['color', 'paintbrush', 'plate'];
+items[3] = 'sprays';
+items.push('new item');
+console.log(items);
+
+// Functions
+function view(name) {
+    console.log("Welcome to Peter's code." + ' ' + name);
+}
+view('He is the best');
+
+function square(number) {
+    return number*number;
+}
+let number = square(2);
+console.log(number);
+
+// Conditional statemen 
+let numberOne = [];
+numberOne = [1,4,1,2,5,2];
+
+if(numberOne[0]==numberOne[3]){
+    console.log('Yes')
+} else {
+    console.log('No')
+}
+
+// While loop
+let i = 0;
+while (i < 5){
+    console.log('number is ' + i);
+    i++;
+}
+
+// Do ...while loop
+let a = 0;
+do{
+    console.log('number is ' + a);
+    a++;
+}
+while(a<6)
+
+// For loop
+let b;
+for(let b = 0; b < 7; b++){
+    console.log('number is ' + a);
+}
+
+// Switch statemen 
+
+let game='cricket';
+switch(game){
+    case 'footbal':
+        console.log('I do not want to watch footbal');
+    break;
+    case 'hokey':
+        console.log('I do not want to watch hokey');
+    break;
+    case 'cricket':
+        console.log('I love cricket');
+    break;
+    default:
+        console.log('Match not found');
+}
