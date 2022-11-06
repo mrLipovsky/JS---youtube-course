50e1cc5d9231412790b04be159bc8c4ed5999ada
mr_lipovsky <90620664+mrLipovsky@users.noreply.github.com>
2022-11-06 13:17:28 +0100

    Add files via upload

---
+++ JS - youtube course/script.js
@@ -0,0 +1,19 @@
+let nameOne = 'peter';
+console.log(nameOne);
+
+// can change
+let ageTwo = 23;
+ageTwo = 21;
+console.log(ageTwo);
+
+// can NOT change
+const ageOne = 22;
+// ageOne = 21;
+console.log(ageOne);
+
+let name = 'Peter'; //string
+let age = 36; //number
+let isApproved = true; //boolien
+let firstName = undefined; //undefined variable 
+let date = null; //null object 
+
